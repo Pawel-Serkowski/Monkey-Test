d24f26974d9b6a0df6f2f96f650f2e49c6d1840f
Pawel-Serkowski <paw.serkowski@gmail.com>
2023-03-30 10:35:18 +0200

    Basic functionality of the game. Getting error message after not correct tile.

--- src/components/boxes/ValueBox.js
+++ src/components/boxes/ValueBox.js
@@ -3,19 +3,23 @@ import React, { useState } from "react";
 import "./ValueBox.css";
 
 const ValueBox = (props) => {
-  const [isActive, setIsActive] = useState();
+  const [isActive, setIsActive] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const tileOnClickHandler = () => {
+    if (props.isAllOpen || isActive) return;
     setIsActive(true);
-    //call testing function
+    setIsError(props.checkingFunc(props.value));
   };
 
   return (
     <div
-      className={`value-box ${isActive ? "active" : ""}`}
+      className={`value-box ${isActive ? "active" : ""} ${
+        isError ? "error" : ""
+      }`}
       onClick={tileOnClickHandler}
     >
-      {isActive ? props.value : ""}
+      {isActive || props.isAllOpen ? props.value : ""}
     </div>
   );
 };
--- src/components/container/BoxContainer.js
+++ src/components/container/BoxContainer.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 import ValueBox from "../boxes/ValueBox";
 
 import "./BoxContainer.css";
 
+const level = 3;
 const tiles = [1, 2, 3, 4];
+const how_many_tiles = tiles.length;
+
 const BoxContainer = () => {
+  const [isAllOpen, setIsAllOpen] = useState(false);
+  const correctNextNumber = useRef(1);
+
   const STYLE = {
-    gridTemplateColumns: "1fr 1fr",
+    gridTemplateColumns: `1fr 1fr`,
     gridTemplateRows: "1fr 1fr",
+  };
+
+  const checkCorrectness = (clicked_number) => {
+    if (clicked_number !== correctNextNumber.current) {
+      setIsAllOpen(true);
+      /*message to player because he lost*/
+      return true; //return this tile is wrong
+    }
+    correctNextNumber.current = correctNextNumber.current + 1;
+    if (clicked_number === how_many_tiles) {
+      alert("You Win!!!"); //player won
+    }
+    return false;
   };
 
   return (
     <section className="box-container" style={STYLE}>
       {tiles.map((value) => {
-        return <ValueBox value={value} key={value} />;
+        return (
+          <ValueBox
+            value={value}
+            key={value}
+            isAllOpen={isAllOpen}
+            checkingFunc={checkCorrectness}
+          />
+        );
       })}
     </section>
   );
